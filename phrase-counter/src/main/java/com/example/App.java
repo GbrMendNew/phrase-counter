cc6c48739ea743d142a8d84a7410376ba6a51003
GabrielRbrMendonca <83738265+GabrielRbrMendonca@users.noreply.github.com>
2026-01-20 18:52:01 -0300

    README adicionado, job.yaml adicionado por causa do cluster (kind)

--- phrase-counter/src/main/java/com/example/App.java
+++ phrase-counter/src/main/java/com/example/App.java
@@ -7,32 +7,29 @@ import com.example.http.HttpContentFetcher;
 import java.util.Scanner;
 
 public class App {
+  public static void main(String[] args) throws Exception {
 
-public static void main(String[] args) throws Exception {
+    String url = System.getenv("URL");
+    String phrase = System.getenv("PHRASE");
 
+    // fallback local 
+    if (url == null || url.isBlank() || phrase == null || phrase.isBlank()) {
+      try (Scanner sc = new Scanner(System.in)) {
+        System.out.print("URL: ");
+        url = sc.nextLine().trim();
 
+        System.out.print("Frase: ");
+        phrase = sc.nextLine().trim();
+      }
+    }
 
-  try (Scanner sc = new Scanner(System.in)) {
-
-    System.out.print("URL: ");
-    String url = sc.nextLine().trim();
-
-    System.out.print("Frase: ");
-    String phrase = sc.nextLine().trim();
-
-    PhraseAnalysisService service =
-        new PhraseAnalysisService(new HttpContentFetcher());
-
+    PhraseAnalysisService service = new PhraseAnalysisService(new HttpContentFetcher());
     PhraseAnalysisResult result = service.analyze(url, phrase);
 
     System.out.println();
-    System.out.printf("\"%s\" => repete %d vezes%n",
-        phrase, result.phraseCount());
-
+    System.out.printf("\"%s\" => repete %d vezes%n", phrase, result.phraseCount());
     result.wordCounts().forEach((word, count) ->
         System.out.printf("\"%s\" => repete %d vezes%n", word, count)
     );
   }
 }
-}
-
